18504e27c5aebbbea680cf31717e7305caadea7b
Justin Tsang <jctsang@usc.edu>
2020-11-04 20:29:38 -0800

    mediaTitle class

---
+++ src/main/java/mediaTitle.java
@@ -0,0 +1,38 @@
+import javax.persistence.*;
+import javax.persistence.Id;
+
+@Entity
+public class mediaTitle {
+    //mediaTitleID, title, genre
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long mediaTitleID;
+
+    private String genre;
+
+    private String title;
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public Long getMediaTitleID() {
+        return mediaTitleID;
+    }
+
+    public void setMediaTitleID(Long ID) {
+        this.mediaTitleID = ID;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public void setPostID(String title) {
+        this.title = title;
+    }
+}
